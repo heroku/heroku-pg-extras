e3e642c4b5dd437e5cdfa27165a5b1033a5f2d88
Will Leinweber <will@bitfission.com>
2012-09-10 16:28:51 -0700

    fix readme

--- init.rb
+++ init.rb
@@ -87,6 +87,35 @@ class Heroku::Command::Pg < Heroku::Command::Base
     exec_sql(sql, find_uri)
   end
 
+  # pg:mandelbrot [database]
+  #
+  # show the mandelbrot set
+  #
+  def mandelbrot
+    sql = %q(WITH RECURSIVE Z(IX, IY, CX, CY, X, Y, I) AS (
+                SELECT IX, IY, X::float, Y::float, X::float, Y::float, 0
+                FROM (select -2.2 + 0.031 * i, i from generate_series(0,101) as i) as xgen(x,ix),
+                     (select -1.5 + 0.031 * i, i from generate_series(0,101) as i) as ygen(y,iy)
+                UNION ALL
+                SELECT IX, IY, CX, CY, X * X - Y * Y + CX AS X, Y * X * 2 + CY, I + 1
+                FROM Z
+                WHERE X * X + Y * Y < 16::float
+                AND I < 100
+          )
+    SELECT array_to_string(array_agg(SUBSTRING(' .,,,-----++++%%%%@@@@#### ', LEAST(GREATEST(I,1),27), 1)),'')
+    FROM (
+          SELECT IX, IY, MAX(I) AS I
+          FROM Z
+          GROUP BY IY, IX
+          ORDER BY IY, IX
+         ) AS ZT
+    GROUP BY IY
+    ORDER BY IY
+)
+
+    exec_sql(sql, find_uri)
+  end
+
   def find_uri
     attachment = hpg_resolve(shift_argument, "DATABASE_URL")
     if attachment.kind_of? Array
