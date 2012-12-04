457d104754c1ee9e17dd3e85dd813a99909d2dae
Harold Giménez <harold.gimenez@gmail.com>
2012-12-04 12:11:50 -0800

    Fix division by zero

--- init.rb
+++ init.rb
@@ -31,7 +31,10 @@ class Heroku::Command::Pg < Heroku::Command::Base
   def indexusage
   sql = %q(SELECT
          relname,
-         100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used,
+         CASE idx_scan
+           WHEN 0 THEN 'Insufficient data'
+           ELSE (100 * idx_scan / (seq_scan + idx_scan))::text
+         END percent_of_times_index_used,
          n_live_tup rows_in_table
        FROM
          pg_stat_user_tables
