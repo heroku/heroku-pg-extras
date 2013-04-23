b6f6584cf7ef04b6552de1cb9f02fc360b42b5f5
Maciek Sakrejda <maciek@heroku.com>
2013-04-23 15:20:44 -0700

    More CTEs

--- init.rb
+++ init.rb
@@ -302,6 +302,24 @@ class Heroku::Command::Pg < Heroku::Command::Base
     sql = %q(
         WITH constants AS (
           SELECT current_setting('block_size')::numeric AS bs, 23 AS hdr, 4 AS ma
+        ), bloat_info AS (
+          SELECT
+            ma,bs,schemaname,tablename,
+            (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
+            (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
+          FROM (
+            SELECT
+              schemaname, tablename, hdr, ma, bs,
+              SUM((1-null_frac)*avg_width) AS datawidth,
+              MAX(null_frac) AS maxfracsum,
+              hdr+(
+                SELECT 1+count(*)/8
+                FROM pg_stats s2
+                WHERE null_frac<>0 AND s2.schemaname = s.schemaname AND s2.tablename = s.tablename
+              ) AS nullhdr
+            FROM pg_stats s, constants
+            GROUP BY 1,2,3,4,5
+          ) AS foo
         )
         SELECT
           tablename as table_name,
@@ -317,27 +335,9 @@ class Heroku::Command::Pg < Heroku::Command::Base
               (CASE WHEN datahdr%ma=0 THEN ma ELSE datahdr%ma END))+nullhdr2+4))/(bs-20::float)) AS otta,
             COALESCE(c2.relname,'?') AS iname, COALESCE(c2.reltuples,0) AS ituples, COALESCE(c2.relpages,0) AS ipages,
             COALESCE(CEIL((c2.reltuples*(datahdr-12))/(bs-20::float)),0) AS iotta -- very rough approximation, assumes all cols
-          FROM (
-            SELECT
-              ma,bs,schemaname,tablename,
-              (datawidth+(hdr+ma-(case when hdr%ma=0 THEN ma ELSE hdr%ma END)))::numeric AS datahdr,
-              (maxfracsum*(nullhdr+ma-(case when nullhdr%ma=0 THEN ma ELSE nullhdr%ma END))) AS nullhdr2
-            FROM (
-              SELECT
-                schemaname, tablename, hdr, ma, bs,
-                SUM((1-null_frac)*avg_width) AS datawidth,
-                MAX(null_frac) AS maxfracsum,
-                hdr+(
-                  SELECT 1+count(*)/8
-                  FROM pg_stats s2
-                  WHERE null_frac<>0 AND s2.schemaname = s.schemaname AND s2.tablename = s.tablename
-                ) AS nullhdr
-              FROM pg_stats s, constants
-              GROUP BY 1,2,3,4,5
-            ) AS foo
-          ) AS rs
-          JOIN pg_class cc ON cc.relname = rs.tablename
-          JOIN pg_namespace nn ON cc.relnamespace = nn.oid AND nn.nspname = rs.schemaname AND nn.nspname <> 'information_schema'
+          FROM bloat_info
+          JOIN pg_class cc ON cc.relname = bloat_info.tablename
+          JOIN pg_namespace nn ON cc.relnamespace = nn.oid AND nn.nspname = bloat_info.schemaname AND nn.nspname <> 'information_schema'
           LEFT JOIN pg_index i ON indrelid = cc.oid
           LEFT JOIN pg_class c2 ON c2.oid = i.indexrelid
         ) AS sml
