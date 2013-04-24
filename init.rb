987fa10823c832dbc760b1cefcbb080ae7d08e44
Maciek Sakrejda <maciek@heroku.com>
2013-04-24 12:20:00 -0700

    Add query to show vacuum status.
    Based on http://dba.stackexchange.com/a/35234 but including
    per-table settings

--- init.rb
+++ init.rb
@@ -364,6 +364,51 @@ class Heroku::Command::Pg < Heroku::Command::Base
     puts exec_sql(sql)
   end
 
+  # pg:vacuum_stats [DATABASE]
+  #
+  # Show dead rows and whether an automatic vacuum is expected to be triggered
+  #
+  def vacuum_stats
+    sql = %q(
+      WITH table_opts AS (
+        SELECT
+          pg_class.oid, relname, nspname, array_to_string(reloptions, '') AS relopts
+        FROM
+           pg_class INNER JOIN pg_namespace ns ON relnamespace = ns.oid
+      ), vacuum_settings AS (
+        SELECT
+          oid, relname, nspname,
+          CASE
+            WHEN relopts LIKE '%autovacuum_vacuum_threshold%'
+              THEN regexp_replace(relopts, '.*autovacuum_vacuum_threshold=([0-9.]+).*', E'\\1')::integer
+              ELSE current_setting('autovacuum_vacuum_threshold')::integer
+            END AS autovacuum_vacuum_threshold,
+          CASE
+            WHEN relopts LIKE '%autovacuum_vacuum_scale_factor%'
+              THEN regexp_replace(relopts, '.*autovacuum_vacuum_scale_factor=([0-9.]+).*', E'\\1')::real
+              ELSE current_setting('autovacuum_vacuum_scale_factor')::real
+            END AS autovacuum_vacuum_scale_factor
+        FROM
+          table_opts
+      )
+      SELECT
+        vacuum_settings.nspname AS schema,
+        vacuum_settings.relname AS table,
+        to_char(psut.last_vacuum, 'YYYY-MM-DD HH24:MI') AS last_vacuum,
+        to_char(psut.last_autovacuum, 'YYYY-MM-DD HH24:MI') AS last_autovacuum,
+        to_char(pg_class.reltuples, '9G999G999G999') AS rowcount,
+        to_char(psut.n_dead_tup, '9G999G999G999') AS dead_rowcount,
+        to_char(autovacuum_vacuum_threshold
+             + (autovacuum_vacuum_scale_factor::numeric * pg_class.reltuples), '9G999G999G999') AS autovacuum_threshold,
+        autovacuum_vacuum_threshold + (autovacuum_vacuum_scale_factor::numeric * pg_class.reltuples) < psut.n_dead_tup AS expect_autovacuum
+      FROM
+        pg_stat_user_tables psut INNER JOIN pg_class ON psut.relid = pg_class.oid
+          INNER JOIN vacuum_settings ON pg_class.oid = vacuum_settings.oid
+      ORDER BY 1
+    )
+    puts exec_sql(sql)
+  end
+
   private
 
   def find_uri
