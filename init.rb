d943c819298ed7c60542956a980ec2184a8ee2b6
Clint Shryock <clint@ctshryock.com>
2014-01-28 15:46:33 -0800

    Remove pgbackups:transfer from pg-extras
    Once heroku/heroku#1015 is merged, we'll want to remove this

--- init.rb
+++ init.rb
@@ -6,7 +6,6 @@ The heroku-pg-extras plugin was not loaded.
 It requires Heroku CLI version >= 3.0.0. You are using #{Heroku::VERSION}.
 EOM
 else
- require File.expand_path('lib/heroku/command/pgbackups', File.dirname(__FILE__))
  require File.expand_path('lib/heroku/client/heroku_postgresql', File.dirname(__FILE__))
  require File.expand_path('lib/heroku/command/pg', File.dirname(__FILE__))
 end
