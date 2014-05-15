cafea80f57a93f41f93b9709f7f2985daebfdcd4
Will Leinweber <will@bitfission.com>
2014-05-15 14:22:50 -0700

    prevent division by zero error

--- lib/heroku/command/pg.rb
+++ lib/heroku/command/pg.rb
@@ -189,12 +189,12 @@ class Heroku::Command::Pg < Heroku::Command::Base
     sql = %q(
       SELECT
         'index hit rate' AS name,
-        (sum(idx_blks_hit)) / sum(idx_blks_hit + idx_blks_read) AS ratio
+        (sum(idx_blks_hit)) / nullif(sum(idx_blks_hit + idx_blks_read),0) AS ratio
       FROM pg_statio_user_indexes
       UNION ALL
       SELECT
        'table hit rate' AS name,
-        sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) AS ratio
+        sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read),0) AS ratio
       FROM pg_statio_user_tables;
     )
 
