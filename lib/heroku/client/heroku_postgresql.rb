cafebdc367e3a17d757a5731f8cfed0f43cb4e76
Will Leinweber <will@bitfission.com>
2014-07-16 15:13:15 -0700

    Move :upgrade and :maintenance to the main CLI

--- lib/heroku/client/heroku_postgresql.rb
+++ lib/heroku/client/heroku_postgresql.rb
@@ -1,24 +1,4 @@
 class Heroku::Client::HerokuPostgresql
-  def upgrade
-    http_post "#{resource_name}/upgrade"
-  end
-
-  def upgrade_status
-    http_get "#{resource_name}/upgrade_status"
-  end
-
-  def maintenance_info
-    http_get "#{resource_name}/maintenance"
-  end
-
-  def maintenance_run
-    http_post "#{resource_name}/maintenance"
-  end
-
-  def maintenance_window_set(description)
-    http_put "#{resource_name}/maintenance_window", 'description' => description
-  end
-
   def incidents
     http_get "#{resource_name}/incidents"
   end
