e85b7363fe8b826c6d1da3732f0d7f0b35ec42b1
Maciek Sakrejda <maciek@heroku.com>
2014-07-21 13:01:06 -0700

    Move backups stuff to its own files as per suggestion

--- lib/heroku/client/heroku_postgresql.rb
+++ lib/heroku/client/heroku_postgresql.rb
@@ -22,43 +22,4 @@ class Heroku::Client::HerokuPostgresql
   def incidents
     http_get "#{resource_name}/incidents"
   end
-
-  def backups
-    http_get "#{resource_name}/transfers"
-  end
-
-  def backups_get(id, verbose=false)
-    http_get "#{resource_name}/transfers/#{URI.encode(id)}?verbose=#{verbose}"
-  end
-
-  def backups_capture
-    http_post "#{resource_name}/backups"
-  end
-
-  def backups_restore(backup_url)
-    http_post "#{resource_name}/restores", 'backup_url' => backup_url
-  end
-
-  def backups_delete(id)
-    http_delete "#{resource_name}/backups/#{URI.encode(id)}"
-  end
-
-  def pg_copy(source_name, source_url, target_name, target_url)
-    http_post "#{resource_name}/transfers", {
-      'from_name' => source_name,
-      'from_url' => source_url,
-      'to_name' => target_name,
-      'to_url' => target_url,
-    }
-  end
-
-  private
-
-  def http_delete(path)
-    checking_client_version do
-      response = heroku_postgresql_resource[path].delete
-      display_heroku_warning response
-      sym_keys(json_decode(response.to_s))
-    end
-  end
 end
