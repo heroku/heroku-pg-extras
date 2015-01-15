cafe93b315f63ade4eebf083a4c5915d9eed5139
Will Leinweber <will@bitfission.com>
2015-01-15 13:12:00 -0800

    make it clearer that the exec time is the total time in outliers

--- lib/heroku/command/pg.rb
+++ lib/heroku/command/pg.rb
@@ -578,7 +578,7 @@ class Heroku::Command::Pg < Heroku::Command::Base
     end
     sql = %Q(
         SELECT #{qstr} AS qry,
-        interval '1 millisecond' * total_time AS exec_time,
+        interval '1 millisecond' * total_time AS total_exec_time,
         to_char((total_time/sum(total_time) OVER()) * 100, 'FM90D0') || '%'  AS prop_exec_time,
         to_char(calls, 'FM999G999G999G990') AS ncalls,
         interval '1 millisecond' * (blk_read_time + blk_write_time) AS sync_io_time
