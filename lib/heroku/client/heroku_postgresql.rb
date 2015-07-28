6bf9bdb794555c3f3960857e2af0fb5ce4dfd992
Timothée Peignier <timothee.peignier@tryphon.org>
2015-07-28 14:07:56 -0700

    Remove FDW methods

--- lib/heroku/client/heroku_postgresql.rb
+++ lib/heroku/client/heroku_postgresql.rb
@@ -2,16 +2,4 @@ class Heroku::Client::HerokuPostgresql
   def incidents
     http_get "#{resource_name}/incidents"
   end
-
-  def fdw_list
-    http_get "#{resource_name}/links"
-  end
-
-  def fdw_set(target)
-    http_post "#{resource_name}/links", 'target' => target
-  end
-
-  def fdw_delete(id)
-    http_delete "#{resource_name}/links/#{id}"
-  end
 end
