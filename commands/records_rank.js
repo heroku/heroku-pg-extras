38b6b2407e097b69cbf710103ee3f7694e26e04b
Jeff Dickey <dickeyxxx@gmail.com>
2016-08-02 10:08:39 -0700

    added pg:records-rank

---
+++ commands/records_rank.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const co = require('co')
+const cli = require('heroku-cli-util')
+const pg = require('heroku-pg')
+
+function * run (context, heroku) {
+  let db = yield pg.fetcher(heroku).database(context.app, context.args.database)
+
+  let query = `
+SELECT
+  relname AS name,
+  n_live_tup AS estimated_count
+FROM
+  pg_stat_user_tables
+ORDER BY
+  n_live_tup DESC;
+`
+
+  let output = yield pg.psql.exec(db, query)
+  process.stdout.write(output)
+}
+
+const cmd = {
+  topic: 'pg',
+  description: 'show all tables and the number of rows in each ordered by number of rows descending',
+  needsApp: true,
+  needsAuth: true,
+  args: [{name: 'database', optional: true}],
+  run: cli.command(co.wrap(run))
+}
+
+module.exports = [
+  Object.assign({command: 'records-rank'}, cmd),
+  Object.assign({command: 'records_rank', hidden: true}, cmd)
+]
