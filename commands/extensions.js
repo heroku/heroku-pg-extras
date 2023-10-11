4e209da913af95151ca2de7cd9d7fe50901b120d
Jon Daniel <jon.daniel@salesforce.com>
2023-10-11 13:41:54 -0400

    Fixing pg:extensions for essential plans
    Previously essential plans caused the pg:extensions command to break.
    They'll now run the correct query to list extensions.

--- commands/extensions.js
+++ commands/extensions.js
@@ -3,16 +3,25 @@
 const co = require('co')
 const cli = require('heroku-cli-util')
 const pg = require('@heroku-cli/plugin-pg-v5')
+const util = require('../lib/util')
 
 function * run (context, heroku) {
   const db = yield pg.fetcher(heroku).database(context.app, context.args.database)
 
-  const query = `
-SELECT * FROM pg_available_extensions WHERE name IN (SELECT unnest(string_to_array(current_setting('extwlist.extensions'), ',')))
-`
+  if (util.essentialNumPlan(db.attachment.addon)) {
+    const query = `SELECT *
+                   FROM pg_available_extensions
+                   WHERE name IN (SELECT unnest(string_to_array(current_setting('rds.extensions'), ', ')))`
 
-  const output = yield pg.psql.exec(db, query)
-  process.stdout.write(output)
+    const output = yield pg.psql.exec(db, query)
+    process.stdout.write(output)
+  } else {
+    const query = `SELECT *
+                   FROM pg_available_extensions
+                   WHERE name IN (SELECT unnest(string_to_array(current_setting('extwlist.extensions'), ',')))`
+    const output = yield pg.psql.exec(db, query)
+    process.stdout.write(output)
+  }
 }
 
 const cmd = {
