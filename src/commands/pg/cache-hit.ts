f5ab5166811d887e1b84222443ad8d2a2032c472
Justin Wilaby <jwilaby@salesforce.com>
2025-08-06 14:45:02 -0700

    feat: migrate the cache-hit command to esm using oclif@4.x

---
+++ src/commands/pg/cache-hit.ts
@@ -0,0 +1,40 @@
+import {utils} from '@heroku/heroku-cli-util'
+import {Command, flags} from '@heroku-cli/command'
+import {Args} from '@oclif/core'
+import heredoc from 'tsheredoc'
+
+export default class CacheHit extends Command {
+  static args = {
+    database: Args.string({description: 'database to run command against', required: false}),
+  }
+
+  static description = 'show index and table hit rate'
+
+  static flags = {
+    app: flags.app({required: true}),
+    remote: flags.remote(),
+  }
+
+  private readonly query = heredoc.default`
+SELECT
+  'index hit rate' AS name,
+  (sum(idx_blks_hit)) / nullif(sum(idx_blks_hit + idx_blks_read),0) AS ratio
+FROM pg_statio_user_indexes
+UNION ALL
+SELECT
+ 'table hit rate' AS name,
+  sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read),0) AS ratio
+FROM pg_statio_user_tables;
+  `
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(CacheHit)
+    const {app: appId} = flags
+    const {database: attachmentId} = args
+
+    const dbConnectionDetails = await utils.pg.fetcher.database(this.heroku, appId, attachmentId)
+
+    const output = await utils.pg.psql.exec(dbConnectionDetails, this.query)
+    process.stdout.write(output)
+  }
+}
