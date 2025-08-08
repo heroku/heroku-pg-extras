2e8c21752d8487286f5b2a1af4efa9bac0778261
Michael Malave <michael.malave@salesforce.com>
2025-08-08 14:16:29 -0700

    removing untracked files

--- dist/lib/util.d.ts
+++
@@ -1,7 +0,0 @@
-import type { AddOnWithRelatedData } from '@heroku/heroku-cli-util/dist/types/pg/data-api.js';
-import type { ConnectionDetailsWithAttachment } from '@heroku/heroku-cli-util/dist/types/pg/tunnel.js';
-export declare function ensurePGStatStatement(connectionDetails: ConnectionDetailsWithAttachment): Promise<void>;
-export declare function ensureEssentialTierPlan(db: AddOnWithRelatedData): void;
-export declare function essentialNumPlan(db: AddOnWithRelatedData): boolean;
-export declare function newTotalExecTimeField(connectionDetails: ConnectionDetailsWithAttachment): Promise<boolean>;
-export declare function newBlkTimeFields(connectionDetails: ConnectionDetailsWithAttachment): Promise<boolean>;
